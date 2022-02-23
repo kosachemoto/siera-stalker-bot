ede36b541774c253cd099aadbab6eec5777a44b7
Nikita Kosachev <kosachemoto@yandex-team.ru>
2022-02-23 13:31:45 +0300

    Добавил типизированный .env

---
+++ src/common/env.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+
+type Env = {
+    BOT_TOKEN: string;
+    CHAT_ID: string;
+    CRON_TIME: string;
+    DEVIATION_RANGE: number;
+}
+
+export const {
+    BOT_TOKEN,
+    CHAT_ID,
+    CRON_TIME,
+    DEVIATION_RANGE,
+}: Env = {
+    BOT_TOKEN: process.env.BOT_TOKEN || '',
+    CHAT_ID: process.env.CHAT_ID || '',
+    CRON_TIME: process.env.CRON_TIME || '',
+    DEVIATION_RANGE: Number(process.env.CRON_TIME || '0'),
+}
