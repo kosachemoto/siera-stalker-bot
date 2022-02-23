e2b7f8a214c0ea0804be082145e759d827a8f458
Nikita Kosachev <kosachemoto@yandex-team.ru>
2022-02-23 13:30:53 +0300

    Добавил модуль для работы с локальными данными

---
+++ src/data-storage/index.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import util from 'util';
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+export class DataStorage<T> {
+    static fileName: string = './previous-response-data.json';
+
+    private value?: Promise<T | undefined>;
+
+    constructor() {
+        this.value = readFile(DataStorage.fileName, {
+            flag: 'a+'
+        }).then<T>((data) => {
+            return JSON.parse(data.toString());
+        }).catch(() => undefined);
+    }
+
+    async get() {
+        return readFile(DataStorage.fileName, {
+            flag: 'a+'
+        }).then<T>((data) => {
+            return JSON.parse(data.toString());
+        }).catch(() => undefined);
+    }
+
+    async set(value: T) {
+        this.value = writeFile(DataStorage.fileName, JSON.stringify(value))
+            .then(() => value)
+            .catch(() => this.value);
+
+        return this.value;
+    }
+}
